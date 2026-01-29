1c68d6a898dd8ecff08751c14b07a148e65a0ef8
Jean Santana <santanajean@hotmail.com>
2026-01-29 03:00:18 +0000

    added gitignore

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <?php include ('head.php');?>
     <title>Jeanss90.com</title>
 </head>
-<body><img src="img/logo-white.svg" alt="jeanss90.com">
+<body>
+    <img src="img/logo-white.svg" alt="jeanss90.com">
     <h1>Jeanss90.com</h1>
     <p>This website is under construction...</p>
 </body>
