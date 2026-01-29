f9ce4bfeefaf7b262a583eeef80269493323300f
Jean Santana <santanajean@hotmail.com>
2026-01-29 02:03:22 +0000

    fix commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include ('head.php');?>
+    <title>Jeanss90.com</title>
+</head>
+<body><img src="img/logo-white.svg" alt="jeanss90.com">
+    <h1>Jeanss90.com</h1>
+    <p>This website is under construction...</p>
+</body>
+</html>
